7d5adad0d416949cd861a25715cdcccc6b038fe7
Orlando Andrade <orlando.andrade1995@gmail.com>
2018-08-25 19:48:23 -0300

    Arreglo interfaz

--- GPI/models.py
+++ GPI/models.py
@@ -183,6 +183,8 @@ class SolicitudMaterial(models.Model):
     trabajadorobra = models.ForeignKey(TrabajadorObra, on_delete=models.CASCADE)
     obra = models.ForeignKey(Obra, on_delete=models.CASCADE)
     estado_solicitud = models.CharField(max_length=20, default='no atendida' ,choices=ESTADO_CHOICES, null = True, blank=True)
+    def __str__(self):
+        return self.numero_orden
 
 
 class MaterialSolicitado(models.Model):
