eccf6e4ae42d66eb131086c348af3a87b8a0dcaa
Orlando Andrade <orlando.andrade1995@gmail.com>
2018-08-29 15:22:37 -0300

    Actualizacion tablas

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'Yayo_11.',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
