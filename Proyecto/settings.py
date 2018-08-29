a61d86df011fdc5f8a2e475430feeffbacfdca54
jcastillo <juan.castillog@sansano.usm.cl>
2018-08-29 16:44:10 -0300

    arreglo url stock

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER': 'root',
-        'PASSWORD': 'Yayo_11.',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
